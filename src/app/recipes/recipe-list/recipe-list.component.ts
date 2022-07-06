90eeef1b3e5838f4ab1f7427b77e51bb0dc3e103
artiumangpatel <aartiumangpatel@gamil.com>
2022-07-06 16:51:37 +0530

    manage recipe in recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
 import{Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,17 +7,27 @@ import{Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-   recipes:Recipe[]=[
-     new Recipe('A Test Recipe',  'This is a simply a test', 'https://thumbs.dreamstime.com/z/indian-food-29155366.jpg' ),
-     new Recipe('A Test Recipe',  'This is a sweet a test', 'https://www.sugarfree-india.com/images/uploads/recipe-images/Sugar-Free-Rabdi-Recipe.jpg' )
 
-    ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
+  constructor(private recipeService:RecipeService) { }
+  
+  recipes:Recipe[]=[];          //use blank array for store
 
-    @Output() recipeWasSelected = new EventEmitter<Recipe>();
-   constructor() { }
+  // remove array  [
+  //    new Recipe('A Test Recipe',  'This is a simply a test', 'https://thumbs.dreamstime.com/z/indian-food-29155366.jpg' ),
+  //    new Recipe('A Test Recipe',  'This is a sweet a test', 'https://www.sugarfree-india.com/images/uploads/recipe-images/Sugar-Free-Rabdi-Recipe.jpg' )
 
-  ngOnInit(): void {
+  //   ];
+
+
+  ngOnInit(): void 
+  {
+    this.recipes = this.recipeService.getRecipes();
   }
-  onRecipeSelected(recipe:Recipe){
-this.recipeWasSelected.emit(recipe);}
+
+  onRecipeSelected(recipe:Recipe)
+  {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
---
+++ src/app/services/recipe.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private recipes:Recipe[]=[
+    new Recipe('A Test Recipe',  'This is a simply a test', 'https://thumbs.dreamstime.com/z/indian-food-29155366.jpg' ),
+    new Recipe('A Test Recipe',  'This is a sweet a test', 'https://www.sugarfree-india.com/images/uploads/recipe-images/Sugar-Free-Rabdi-Recipe.jpg' )
+
+   ];
+  constructor() { }
+  getRecipes(){
+    return this.recipes.slice();
+  }
+}

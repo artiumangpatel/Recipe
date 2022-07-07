40c5e4bdbb91885b9208aaeec07ad4b974ef9f62
artiumangpatel <aartiumangpatel@gamil.com>
2022-07-07 16:16:18 +0530

    Adding Ingrediants to Recipe

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,17 @@
+import { Ingredients } from "./shared/ingredients.model";
+
 export class Recipe {
  public name:string;
  public description:string;
  public imagePath:string;
-
-constructor(name: string, desc: string,imagePath: string )
+ public ingrediants:Ingredients[];
+constructor(name: string, desc: string,imagePath: string,ingredients:Ingredients[] )
 {
 this.name=name;
 this.description=desc;
 this.imagePath=imagePath;
+this.ingrediants=ingredients;
  }
-}
+ 
+ }
+
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,13 +1,23 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
+import { Ingredients } from '../recipes/shared/ingredients.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
   private recipes:Recipe[]=[
-    new Recipe('A Test Recipe',  'This is a simply a test', 'https://thumbs.dreamstime.com/z/indian-food-29155366.jpg' ),
-    new Recipe('A Test Recipe',  'This is a sweet a test', 'https://www.sugarfree-india.com/images/uploads/recipe-images/Sugar-Free-Rabdi-Recipe.jpg' )
+    new Recipe('A Test Recipe', 
+     'This is a simply a test', 
+     'https://thumbs.dreamstime.com/z/vegetarian-burger-home-made-chickpea-cutlet-vegetables-veg-concept-copy-space-fresh-153187934.jpg'
+     ,[new Ingredients('aloo-tikki',1),
+       new Ingredients('lecctus',2)] ),
+    new Recipe('A Test Recipe',  
+    'This is a sweet a test',
+     'https://www.sugarfree-india.com/images/uploads/recipe-images/Sugar-Free-Rabdi-Recipe.jpg' 
+     ,
+     [new Ingredients('milk',6),
+     new Ingredients('sugar',2)] )
 
    ];
   constructor() { }

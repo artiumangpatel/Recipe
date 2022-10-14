599e8884757616e1613414858cbf73a623290169
artiumangpatel <aartiumangpatel@gmail.com>
2022-10-14 15:03:30 +0530

    clear Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -50,6 +50,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
     form.reset();
   }
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
